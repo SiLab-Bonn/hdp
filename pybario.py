35d6cba93e84190a1409ce0cb9a11c156043c40b
silab64 <silab64@silab64-OptiPlex-3060>
2019-09-11 11:55:32 -0400

    Noise hit detection

--- pybario.py
+++ pybario.py
@@ -4,6 +4,8 @@
 import zmq
 import numpy as np
 
+_MAX_NOISE_HITS = 10
+
 # Copied from pybar.daq.readout_utils
 def is_data_record(value):
     return np.logical_and(np.logical_and(np.less_equal(np.bitwise_and(value, 0x00FE0000), 0x00A00000),
@@ -12,13 +14,13 @@ def is_data_record(value):
                                                         np.not_equal(np.bitwise_and(value, 0x0001FF00), 0x00000000)))
     
 # Based on pybar.daq.readout_utils.get_col_row_iterator_from_data_records
-def col_row_pairs(words, max_hits):
-    data_records = words[:100][is_data_record(words[:100])]
+def col_row_pairs(words, max_hits, noise_hits):
+    data_records = words[:][is_data_record(words[:])]
     cols, rows = np.right_shift(np.bitwise_and(data_records, 0x00FE0000), 17), np.right_shift(np.bitwise_and(data_records, 0x0001FF00), 8)
-    for i, (col, row) in enumerate(zip(cols, rows)):
-        if i > max_hits:
-            return
-        yield col, row
+    hits = zip(cols, rows)
+    hits = [hit for hit in hits if hit not in noise_hits]
+    return hits[:max_hits]
+
 
 class IO(object):
     ''' Analyze pybar data '''
@@ -26,6 +28,7 @@ class IO(object):
         self.sockets = []
         context = zmq.Context()
         self.max_hits = max_hits
+        self.last_hits = [[], []]
         for address in addresses:
             s = context.socket(zmq.SUB)  # subscriber
             s.setsockopt(zmq.SUBSCRIBE, b'')  # do not filter any data
@@ -35,9 +38,10 @@ class IO(object):
     def get_module_hits(self):
         ''' Called on app update to fetch zmq data '''
         hits = []
-        for socket in self.sockets:
+        for i, socket in enumerate(self.sockets):
             try:
                 meta_data = socket.recv_json(flags=zmq.NOBLOCK)
+                # print i, meta_data
             except zmq.Again:
                 hits.append(None)
                 pass
@@ -49,7 +53,12 @@ class IO(object):
                     dtype = meta_data.pop('dtype')
                     shape = meta_data.pop('shape')
                     data_array = np.frombuffer(data, dtype=dtype).reshape(shape) 
-                    hits.append(col_row_pairs(data_array, max_hits=self.max_hits))
+                    h = col_row_pairs(data_array, max_hits=self.max_hits, noise_hits=self.last_hits[i])
+                    # if h:
+                    #     print i, "HITS", h
+                    self.last_hits[i] = (h + self.last_hits[i])[:self.max_hits]
+                    hits.append(h)
+                    #print "noise_hits", self.last_hits[i]
                 elif name == 'Filename':
                     hits.append(None)
                     print('Start run for module', meta_data)
